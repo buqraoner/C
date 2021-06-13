3f52fea01c25025623c745a0a5235b0dce556a16
buqraoner <83416454+buqraoner@users.noreply.github.com>
2021-06-13 09:09:26 +0300

    Update strupdown.c

--- strupdown.c
+++ strupdown.c
@@ -14,6 +14,46 @@ int main( )
 //harfleri
 //küçük harfe
 //çevirir
+   //  strcmp(a,b);    = iki stringi karşılaştırma  Birinci stringi
+/* İkinci string ile karşılaştırır.
+Eşitse 0, string1 büyükse 0'dan büyük bir değer 
+aksi halde 0'dan küçük bir değer gönderir
+   
+   #include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[10]="denemeabc";
+char b[10];
+int sonuc; 
+{
+printf("Bir cumle giriniz : ");
+gets(b); 
+sonuc = strcmp(a,b);
+if( sonuc==0 ){ 
+puts("İki cumlede ayni ");
+printf("
+\n");
+puts(a);
+puts(b);
+}
+else
+puts("İki cumle ayni degil");
+printf("
+\n");
+puts(a);
+puts(b);
+}
+return 0;
+}
+   
+   
+   
+   
+   */
+   
+   
+   
 
 #include<stdio.h>
 #include<string.h>
@@ -33,6 +73,10 @@ int main( )
     }
 
 // Buyuk harf
+           
+           
+           
+           
 
 
 }
