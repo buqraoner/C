234ff53234f74dab44d211313adbd74045bf3e1f
buqraoner <83416454+buqraoner@users.noreply.github.com>
2021-06-13 09:03:11 +0300

    Add files via upload

---
+++ strupdown.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<string.h>
+int main( )
+{
+   /* char a[20]="ANKARA";
+    printf("\n");
+    printf("Buyuk harfle yazilan  cumle : %s\n", a );
+    printf("Kucuk harfedonusturulen cumle: %s\n", strlwr(a) );
+    return 0;
+
+    */
+//Stringin
+//büyük
+//harfleri
+//küçük harfe
+//çevirir
+
+#include<stdio.h>
+#include<string.h>
+    int main( )
+    {
+        char
+                a[20]="ankara";
+        printf("\n");
+        printf(
+                "Kucuk harfle yazilan
+        cumle : %s\n", a );
+        printf(
+                "Buyuk harfe
+        donusturulen cumle:
+        %s\n", strupr(a) );
+        return 0;
+    }
+
+// Buyuk harf
+
+
+}
